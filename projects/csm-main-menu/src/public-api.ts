9539b2dcbfae51f507880d3cf06c0284858b645f
Daniel Franco <dafrannco@gmail.com>
2020-01-22 19:26:54 -0300

    Updating Version Number

--- projects/csm-main-menu/src/public-api.ts
+++ projects/csm-main-menu/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of csm-main-menu
  */
 
+require('../assets/styles/style.scss');
+
 export * from './lib/csm-main-menu.component';
 export * from './lib/csm-main-menu.module';
 export * from './interfaces';
